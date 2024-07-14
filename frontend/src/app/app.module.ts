37b512b609e71b0903644b7ad4b109626896c6cf
islemabderahmen <islem.abderahmen@esprit.tn>
2024-07-14 23:21:35 +0100

    integration user template without routing

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { SubjectsComponent } from './components/subjects/subjects.component';
+import { ExpertsComponent } from './components/experts/experts.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    CoursesComponent,
+    SubjectsComponent,
+    ExpertsComponent,
+    IntroductionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
