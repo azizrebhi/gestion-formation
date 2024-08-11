ac15c53d0bc8ed0252c8f89ad2f066501bb67d43
aziz rebhi <mohamedaziz.rebhi@ensi-uma.tn>
2024-08-11 22:19:50 +0100

    add poll back end

--- backend/src/main/java/com/example/gestionFormation/Service/UserDetailsImpl.java
+++ backend/src/main/java/com/example/gestionFormation/Service/UserDetailsImpl.java
@@ -41,7 +41,7 @@ public class UserDetailsImpl implements UserDetails {
                 .collect(Collectors.toList());
 
         return new UserDetailsImpl(
-                user.getIdUser(),
+                user.getId(),
                 user.getEmail(),
                 user.getFirstName(),
                 user.getPassword(),
